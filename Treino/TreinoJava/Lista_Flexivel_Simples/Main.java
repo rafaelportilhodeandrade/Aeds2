67c3f77daa245a3437fd85f7b968efffb90d4a7c
Rafael Portilho de Andrade <rafaelportilho2000@gmail.com>
2025-10-22 17:03:03 -0300

    Correção

--- Treino/TreinoJava/Lista_Flexivel_Simples/Main.java
+++ Treino/TreinoJava/Lista_Flexivel_Simples/Main.java
@@ -1,96 +1,294 @@
 import java.util.*;
 
-class Celula
+class Celula 
 {
+    //---------- Construtores ----------
     int elemento;
     Celula prox;
 
-    public Celula(int elemento, Celula prox)
+    public Celula(int elemento, Celula prox) 
     {
         this.elemento = elemento;
         this.prox = prox;
     }
 
-    public Celula(int elemento)
+    public Celula(int elemento) 
     {
-        this.elemento = elemento;
-        this.prox = null;
+        this(elemento, null);
     }
 
-    public Celula()
+    public Celula() 
     {
-        this.elemento = 0;
-        this.prox = null;
+        this(0, null);
+    }
+
+    public void setElemento(int elemento) 
+    {
+        this.elemento = elemento;
+    }
+
+    public int getElemento() 
+    {
+        return elemento;
+    }
+
+    public void setProx(Celula prox) 
+    {
+        this.prox = prox;
+    }
+
+    public Celula getProx() 
+    {
+        return prox;
     }
 }
 
-class Fila
+class ListaEncadeada 
 {
     private Celula primeiro;
     private Celula ultimo;
 
     //inicia a celula cabeça
-    public Fila() 
+    public ListaEncadeada() 
     {
         primeiro = new Celula(); 
         ultimo = primeiro;
     }
 
-    // Inserir Inicio
-    public void inserirInicio(int elemento)
+    //---------- Inserir Inicio ----------
+    public void InserirInicio(int elemento) 
     {
         Celula temp = new Celula(elemento, primeiro.prox);
         primeiro.prox = temp;
 
-        if(primeiro == ultimo)
+        if (primeiro == ultimo) 
         {
             ultimo = temp;
         }
-
         temp = null;
     }
 
-    // RemoverInicio
-    public void RemoverInicio()
+    //---------- Inserir Fim ----------
+    public void InserirFim(int elemento)
     {
+        Celula temp = new Celula(elemento, null);
+        ultimo.prox = temp;
+        ultimo = temp;
+        temp = null;
+    }
+
+    //conta o tamanho da lista
+    public int Contador() 
+    {
+        int cont = 0;
+        Celula i = primeiro.prox;
+    
+        while(i != null) 
+        {
+            cont++;
+            i = i.prox;
+        }
+    
+        return cont; 
+    }
+    
+
+    //Insere em qualquer posição da lista
+    public void Inserir(int elemento, int pos) 
+    {
+        int tam = Contador();
+    
+        if (pos < 0 || pos > tam + 1) throw new IndexOutOfBoundsException("Posição inválida");
+    
+        if (pos == 0) InserirInicio(elemento);
+         
+        else if (pos == tam + 1) InserirFim(elemento);
+
+        else 
+        {
+            Celula temp = new Celula(elemento);
+            Celula i = primeiro;
+            for (int j = 0; j < pos - 1; j++) 
+            {
+                i = i.prox;
+            }
+            temp.prox = i.prox;
+            i.prox = temp;
+            temp = i = null;
+        }
+    }
+    
+
+    //Remover inicio alterando a celula cabeça
+    public void RemoverInicio() 
+    {
+        if(primeiro == ultimo) throw new IllegalStateException("lista com apenas um elemento");
+
         Celula temp = primeiro;
-        primeiro= primeiro.prox;
+        primeiro = primeiro.prox;
         temp = temp.prox = null;
     }
 
-    // Mostrar
-    public void mostrar()
+    //Remover o ultimo elemento da lista e se tiver apenas um lança um erro.
+    public void RemoverFim()
     {
-        Celula i = primeiro.prox;
+        if(primeiro == ultimo) throw new IllegalStateException("lista com apenas um elemento");
 
-        for(; i != null; i = i.prox)
+        Celula i = primeiro;
+        for(; i.prox != ultimo; i = i.prox);
+        ultimo = i;
+        ultimo.prox = null;
+    }
+
+    //remove em qualquer posição da lista
+    public void Remover(int pos)
+    {
+        int tam = Contador();
+
+        if(tam == 0) throw new IllegalStateException("lista com apenas um elemento");
+        else if(pos == 0) RemoverInicio();
+        else if(pos == tam) RemoverFim();
+        else
         {
-            System.out.println(i.elemento);
+            Celula i = primeiro;
+            for(int j = 0; j < pos - 1; j++) i = i.prox;
+            Celula temp = i.prox;
+            i.prox = i.prox.prox;
+            temp.prox = null;
+            i = temp = null;
         }
     }
 
+    //---------- ORDENAÇÕES ----------
 
-    // Ordenações
+    //--------- Seleção ---------
+    public void Selecao() 
+    {
+        int tam = Contador();
 
+        if(tam == 0) throw new IllegalStateException("lista com apenas um elemento");
 
+        for (Celula i = primeiro.prox; i != null; i = i.prox) 
+        {
+            Celula menor = i;
+
+            for (Celula j = i.prox; j != null; j = j.prox) 
+            {
+                if (j.elemento < menor.elemento) 
+                {
+                    menor = j;
+                }
+            }
+
+            if (menor != i) 
+            {
+                int temp = i.elemento;
+                i.elemento = menor.elemento;
+                menor.elemento = temp;
+            }
+        }
+    }
+
+    //--------- Inserção ---------
+
+    public void Insercao()
+    {
+        int tam = Contador();
+
+        if(tam == 0) throw new IllegalStateException("lista com apenas um elemento ou nenhum");
+
+        Celula i = primeiro.prox.prox;
+        Celula j;
+
+        while(i != null)
+        {
+            int temp = i.elemento;
+            j = getAnterior(i);
+            
+            while(j != primeiro && j.elemento > temp) 
+            {
+                j.prox.elemento = j.elemento;
+                j = getAnterior(j);
+            }
+            
+            j.prox.elemento = temp;
+            i = i.prox;
+        }
+    }
+
+    private Celula getAnterior(Celula i) {
+        Celula j = primeiro;
+        while(j.prox != i) j = j.prox;
+        return j;
+    }
+
+    //--------- Bolha ---------
+    public void Bolha()
+    {
+        int tam = Contador();
+
+        if(tam == 0) throw new IllegalStateException("lista com apenas um elemento ou nenhum");
+
+        Celula var = ultimo;
+
+        for(int i = 0; i < tam - 1; i++)
+        {
+            for(Celula j = primeiro.prox; j != var; j = j.prox)
+            {
+                if(j.elemento > j.prox.elemento)
+                {
+                    int temp = j.elemento;
+                    j.elemento = j.prox.elemento;
+                    j.prox.elemento = temp;
+                }
+            }
+            var = getAnterior(var);
+        }
+    }
+
+    //---------- ShellSort ----------
+
+    //---------- CountingSort ----------
+
+    //---------- RadixSort ----------
+
+    //--------- Quicksort ---------
+
+    //--------- Mergesort ---------
+
+    //--------- Heapsort ---------
+
+    //---------- Mostrar ----------
+    public void Mostrar()
+    {
+        Celula i = primeiro.prox;
+
+        System.out.println("");
+        while(i != null)
+        {
+            System.out.println(i.elemento);
+            i = i.prox;
+        }
+        System.out.println("");
+    }
 }
 
-public class Main
+public class Main 
 {
     public static void main(String[] args) 
     {
-        Fila fila = new Fila();
+        ListaEncadeada lista = new ListaEncadeada();
 
-        fila.inserirInicio(1);
-        fila.inserirInicio(5);
-        fila.inserirInicio(3);
-        fila.inserirInicio(8);
-        fila.inserirInicio(2);
-        fila.inserirInicio(7);
-        fila.inserirInicio(4);
-        fila.inserirInicio(6);
-        fila.RemoverInicio();
-
-        fila.mostrar();
+        lista.InserirInicio(5);
+        lista.InserirInicio(6);
+        lista.InserirInicio(1);
+        lista.InserirInicio(2);
+        lista.InserirInicio(4);
+        lista.InserirFim(3);
+        lista.Inserir(7,4);
+        //lista.Selecao();
+        //lista.Insercao();
+        lista.Bolha();
+        lista.Mostrar();
     }
 }
